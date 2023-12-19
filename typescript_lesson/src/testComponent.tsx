5e6ea08c7fc6f9d86eaf92f7866cf7fecb7a5909
hideaki.murakami <hideaki.murakami@nijibox.co.jp>
2023-12-19 09:54:36 +0900

    2024/12/19 diary更新

--- typescript_lesson/src/testComponent.tsx
+++ typescript_lesson/src/testComponent.tsx
@@ -12,13 +12,20 @@ interface UserData {
 const TestComponent: React.FC<Props> = (props) => {
   const [count, setCount] = useState<number | null>(0);
   const [user, setUser] = useState<UserData>({ id: 1, name: "dummy" });
+  const [inputData, setInputData] = useState("");
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) =>
+    setInputData(e.target.value);
+
   // setCount(1);
   return (
     <div>
       <h1>{props.text}</h1>
       <h1>{count}</h1>
-      <h1>{user.id}</h1>
-      <h1>{user.name}</h1>
+      {/* <h1>{user.id}</h1>
+      <h1>{user.name}</h1> */}
+      <input type="text" value={inputData} onChange={handleInputChange} />
+      <h1>{inputData}</h1>
     </div>
   );
 };
