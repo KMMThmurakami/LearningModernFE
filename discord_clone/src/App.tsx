c7b6c623bfcbd59d6fb69459e379e0a8bcbeca90
hideaki.murakami <hideaki.murakami@nijibox.co.jp>
2023-12-22 09:56:56 +0900

    サイドバー準備

--- discord_clone/src/App.tsx
+++ discord_clone/src/App.tsx
@@ -1,8 +1,15 @@
 import React from "react";
-import "./App.css";
+import "./App.scss";
+import Sidebar from "./component/sidebar/Sidebar";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      {/* sidebar */}
+      <Sidebar />
+      {/* home */}
+    </div>
+  );
 }
 
 export default App;
