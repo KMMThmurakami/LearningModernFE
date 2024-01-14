694d0c1a7f14612337f68f83804fc08e064c8f71
hideaki.murakami <hideaki.murakami@nijibox.co.jp>
2024-01-15 02:01:08 +0900

    送信箇所

--- discord_clone/src/component/chat/Chat.tsx
+++ discord_clone/src/component/chat/Chat.tsx
@@ -1,6 +1,11 @@
 import React from 'react'
 import "./Chat.scss";
 import ChatHeader from "./ChatHeader";
+import AddCircleIcon from '@mui/icons-material/AddCircle';
+import CardGiftcardIcon from '@mui/icons-material/CardGiftcard';
+import GifBoxIcon from '@mui/icons-material/GifBox';
+import EmojiEmotionsIcon from '@mui/icons-material/EmojiEmotions';
+import SendIcon from '@mui/icons-material/Send';
 
 const Chat = () => {
   return (
@@ -15,7 +20,16 @@ const Chat = () => {
 
       {/* chatInput */}
       <div className="chatInput">
-      chatInput
+        <AddCircleIcon />
+        <form>
+          <input type="text" placeholder='メッセージを送信'/>
+          <div className='chatInputIcons'>
+            <CardGiftcardIcon />
+            <GifBoxIcon />
+            <EmojiEmotionsIcon />
+            <button type='submit' className='chatInputButton'><SendIcon className="chatHeaderIcon" /></button>
+          </div>
+        </form>
       </div>
     </div>
   )
