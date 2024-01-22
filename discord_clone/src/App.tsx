3b033ff0e1c246e98dc334326daa003ebb3c0d2b
hideaki.murakami <hideaki.murakami@nijibox.co.jp>
2024-01-22 19:41:34 +0900

    ログアウト追加

--- discord_clone/src/App.tsx
+++ discord_clone/src/App.tsx
@@ -1,17 +1,37 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.scss";
 import Sidebar from "./component/sidebar/Sidebar";
 import Chat from "./component/chat/Chat";
 import Login from "./component/login/Login";
-import { useAppSelector } from "./app/hooks";
+import { useAppDispatch, useAppSelector } from "./app/hooks";
+import { auth } from "./firebase";
+import { login, logout } from "./features/userSlice";
 
 function App() {
-
   const user = useAppSelector((state) => state.user);
+
+  const dispatch = useAppDispatch();
+
+  // 第二引数は配列、配列の中の変数が更新されるたびにuseEffectの中の関数が発火する
+  useEffect(() => {
+    auth.onAuthStateChanged((loginUser) => {
+      if (loginUser) {
+        dispatch(
+          login({
+            uid: loginUser.uid,
+            photo: loginUser.photoURL,
+            email: loginUser.email,
+            displayName: loginUser.displayName,
+          })
+        );
+      } else {
+        dispatch(logout());
+      }
+    });
+  }, [dispatch]);
 
   return (
     <div className="App">
-
       {user ? (
         <>
           <Sidebar />
--- discord_clone/src/component/sidebar/SidebarFooter.tsx
+++ discord_clone/src/component/sidebar/SidebarFooter.tsx
@@ -3,15 +3,25 @@ import "./SidebarFooter.scss";
 import MicIcon from "@mui/icons-material/Mic";
 import HeadphonesIcon from "@mui/icons-material/Headphones";
 import SettingIcon from "@mui/icons-material/Settings";
+import { auth } from "../../firebase";
+import { useAppSelector } from "../../app/hooks";
 
 function SidebarFooter() {
+  const user = useAppSelector((state) => state.user);
+
   return (
     <div className="sidebarFooter">
       <div className="sidebarAccount">
-        <img src={`${process.env.PUBLIC_URL}/myIcon.png`} alt="" />
+        <img
+          src={user?.photo}
+          width="60px"
+          height="60px"
+          alt=""
+          onClick={() => auth.signOut()}
+        />
         <div className="accountName">
-          <h4>kumamon</h4>
-          <span>#0629</span>
+          <h4>{user?.displayName}</h4>
+          <span>#{user?.uid.substring(0, 4)}</span>
         </div>
       </div>
 
