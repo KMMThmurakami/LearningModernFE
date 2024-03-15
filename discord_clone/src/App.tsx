c3f0f40d6543ac929d34d4f7e569759a3f1195df
hideaki.murakami <hideaki.murakami@nijibox.co.jp>
2024-03-15 21:57:27 +0900

    リロード時に先頭のチャンネルが選択される様に変更

--- discord_clone/src/App.tsx
+++ discord_clone/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.scss";
 import Sidebar from "./component/sidebar/Sidebar";
 import Chat from "./component/chat/Chat";
@@ -8,11 +8,21 @@ import { auth } from "./firebase";
 import { login, logout } from "./features/userSlice";
 import { ErrorBoundary } from "react-error-boundary";
 import { ErrorFallback } from "./utils/errorFallback";
+import useCollection from "./hooks/useCollection"; // import useCollection
+import { setChannelInfo } from "./features/channelSlice";
+
+type Channels = {
+  id: string;
+  channel: {
+    channelName: string;
+  };
+};
 
 function App() {
   const user = useAppSelector((state) => state.user.user);
 
   const dispatch = useAppDispatch();
+  const [initialChannel, setInitialChannel] = useState<Channels | null>(null); // 初期チャンネルの型を修正
 
   // 第二引数は配列、配列の中の変数が更新されるたびにuseEffectの中の関数が発火する
   useEffect(() => {
@@ -31,6 +41,30 @@ function App() {
       }
     });
   }, [dispatch]);
+
+  const { documents: channels } = useCollection("channels"); // Fetch channels
+  const channelsOrderBy = channels.sort((a, b) =>
+    a.channel.channelName.localeCompare(b.channel.channelName)
+  );
+
+  useEffect(() => {
+    // When channels are loaded, select the first one
+    if (channelsOrderBy && channelsOrderBy.length > 0 && !initialChannel) {
+      setInitialChannel(channelsOrderBy[0] as Channels | null); // channelsOrderBy[0] を Channels | null に変換
+    }
+  }, [channelsOrderBy, initialChannel]);
+
+  useEffect(() => {
+    // Set initial channel when it's available
+    if (initialChannel) {
+      dispatch(
+        setChannelInfo({
+          channelId: initialChannel.id,
+          channelName: initialChannel.channel.channelName,
+        })
+      );
+    }
+  }, [initialChannel, dispatch]);
 
   return (
     <div className="App">
